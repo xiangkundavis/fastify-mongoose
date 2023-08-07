98878de812439b31f7aa7d68913ae14657c7fcd0
davis.du <davis.du@merkle.com>
2023-08-07 15:32:03 +0800

    add env

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fastify = require('fastify');
 const mongoose = require('mongoose');
 const noteRoutes = require('./routes/noteRoutes');
-
+const fastify_env = require('@fastify/env');
+require("dotenv").config();
 const app = fastify();
 
 try {
+  console.log(process.env.MONGO_URI,'process.env.MONGO_URI')
   mongoose.connect('mongodb://localhost:27017/notes_db');
 } catch (e) {
   console.error(e);
